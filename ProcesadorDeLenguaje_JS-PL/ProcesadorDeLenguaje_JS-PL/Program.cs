6a30fcfa9c9ff71fb13e1445948abaf5c0cfd883
Carlos Moro Jiménez <krlsmj@protonmail.com>
2019-06-15 20:02:05 +0200

    Respuesta

--- ProcesadorDeLenguaje_JS-PL/ProcesadorDeLenguaje_JS-PL/Program.cs
+++ ProcesadorDeLenguaje_JS-PL/ProcesadorDeLenguaje_JS-PL/Program.cs
@@ -8,6 +8,7 @@ namespace ProcesadorDeLenguaje_JS_PL {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hola mundo");
+            Console.WriteLine("Hola persona");
             Console.ReadKey();
         }
     }
